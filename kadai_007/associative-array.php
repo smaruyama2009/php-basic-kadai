78b0b6dbb120c3d86d06d99fe6bda360c0d028e3
smaruyama2009 <151481610+smaruyama2009@users.noreply.github.com>
2024-01-29 14:05:58 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列の値を出力する
+    $vege_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+    // 配列の値を出力する
+    print_r ($vege_data);
+    
+    ?>
+    </p>
+</body>
+
+</html>
